d7ca191b2a1d95f7defe166983435ff13bb0c91f
vinverma <47500906+vinverma@users.noreply.github.com>
2019-03-10 00:49:35 +0000

    implicit wait added for successful build.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -23,11 +23,17 @@ public class App
 
  driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
  driver.findElement(By.className("v-button")).click();
+  driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
  driver.findElement(By.id("gwt-uid-5")).sendKeys("Edureka");
+  driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
  driver.findElement(By.id("gwt-uid-7")).sendKeys("DevOps");
+  driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
  driver.findElement(By.id("gwt-uid-9")).sendKeys("9834562709");
+  driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
  driver.findElement(By.id("gwt-uid-11")).sendKeys("Devops@edureka.co");
+  driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
  driver.findElement(By.id("gwt-uid-13")).sendKeys("01/01/2015");
+  driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
  driver.findElement(By.className("v-button-primary")).click();
  //Thread.sleep(5000);
  driver.quit();
